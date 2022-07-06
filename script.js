4da6fd2fa341b6dd74212cf66419a42decb6c65a
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-06 14:49:02 +0100

    Capitalised names

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 import pokemonArray from "./data/pokemon.js";
-// ^^ an array of pokemon objects
 
 let cardContainer = document.querySelector(".card-container");
 
 const addCard = (pokemonObject) => {
-    const name = pokemonObject.name;
+    //Capitalise name
+    const lowercaseName = pokemonObject.name;
+    pokemonName = lowercaseName.charAt(0).toUpperCase() + lowercaseName.slice(1);
+
+    //Add card
     cardContainer.innerHTML += `
         <div class="card">
             <img class="card__image" src="${pokemonObject.sprite}"/>
             <div class="card__content">
-                <h1 class="card__heading">${name}</h1>
-                <p class="card__text" id="${name}"></p>
+                <h1 class="card__heading">${pokemonName}</h1>
+                <p class="card__text" id="${pokemonName}"></p>
             </div>
         </div>
     `;
 
-    let cardText = document.querySelector(`#${name}`);
+    //Adjust text based on number of types
+    let cardText = document.querySelector(`#${pokemonName}`);
     
     if (pokemonObject.types.length == 1){
-        cardText.innerText += `${name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} type pokemon.`;
+        cardText.innerText += `${pokemonName} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} type pokemon.`;
     } else if(pokemonObject.types.length == 2){
-        cardText.innerText += `${name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} & ${pokemonObject.types[1]} type pokemon.`;
+        cardText.innerText += `${pokemonName} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} & ${pokemonObject.types[1]} type pokemon.`;
     }
 }
 
