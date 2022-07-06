59b15b8a99ee021f9995e5eca9d697e2c2eabb02
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-06 14:39:31 +0100

    Cards displayed

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-import "./data/pokemon";
+import pokemonArray from "./data/pokemon.js";
 // ^^ an array of pokemon objects
 
 let cardContainer = document.querySelector(".card-container");
 
 const addCard = (pokemonObject) => {
+    const name = pokemonObject.name;
     cardContainer.innerHTML += `
         <div class="card">
             <img class="card__image" src="${pokemonObject.sprite}"/>
             <div class="card__content">
-                <h1 class="card__heading">${pokemonObject.name}</h1>
-                <p class="card__text"></p>
+                <h1 class="card__heading">${name}</h1>
+                <p class="card__text" id="${name}"></p>
             </div>
-        </div>`;
+        </div>
+    `;
 
-    let cardText = document.querySelector(".card__text");
-
+    let cardText = document.querySelector(`#${name}`);
+    
     if (pokemonObject.types.length == 1){
-        cardText.innerText = `${pokemonObject.name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} type pokemon.`;
+        cardText.innerText += `${name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} type pokemon.`;
     } else if(pokemonObject.types.length == 2){
-        cardText.innerText = `${pokemonObject.name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} and ${pokemonObject.types[1]} type pokemon.`;
+        cardText.innerText += `${name} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} & ${pokemonObject.types[1]} type pokemon.`;
     }
 }
+
+const loopArray = () => {
+    for (let i=0; i<pokemonArray.length; i++){
+        addCard(pokemonArray[i]);
+    }
+}
+
+window.addEventListener("load", loopArray);
