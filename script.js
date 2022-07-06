d2cf48f1e17674b34dd181766fd13e0917a7f83e
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-06 15:09:50 +0100

    Made Poke Types text DRY.; cleaned code

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ import pokemonArray from "./data/pokemon.js";
 let cardContainer = document.querySelector(".card-container");
 
 const addCard = (pokemonObject) => {
-    //Capitalise name
+    //Define multi-use variables
     const lowercaseName = pokemonObject.name;
-    pokemonName = lowercaseName.charAt(0).toUpperCase() + lowercaseName.slice(1);
+    const type1 = pokemonObject.types[0];
+
+    //Capitalise name
+    const pokemonName = lowercaseName.charAt(0).toUpperCase() + lowercaseName.slice(1);
 
     //Add card
     cardContainer.innerHTML += `
@@ -13,18 +16,19 @@ const addCard = (pokemonObject) => {
             <img class="card__image" src="${pokemonObject.sprite}"/>
             <div class="card__content">
                 <h1 class="card__heading">${pokemonName}</h1>
-                <p class="card__text" id="${pokemonName}"></p>
+                <p class="card__text" id="${pokemonName}">${pokemonName} (#${pokemonObject.id}) is a ${type1} type pokemon.</p>
             </div>
         </div>
     `;
 
-    //Adjust text based on number of types
-    let cardText = document.querySelector(`#${pokemonName}`);
-    
-    if (pokemonObject.types.length == 1){
-        cardText.innerText += `${pokemonName} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} type pokemon.`;
-    } else if(pokemonObject.types.length == 2){
-        cardText.innerText += `${pokemonName} (#${pokemonObject.id}) is a ${pokemonObject.types[0]} & ${pokemonObject.types[1]} type pokemon.`;
+    //Adjust text if dual-type
+    const cardText = document.querySelector(`#${pokemonName}`);
+
+    if (pokemonObject.types.length == 2){
+        const type1Position = cardText.innerText.indexOf(`${type1}`);
+        cardText.innerText = cardText.innerText.slice(0, type1Position+type1.length)
+                                + ` & ${pokemonObject.types[1]}` 
+                                + cardText.innerText.slice(type1Position+type1.length);
     }
 }
 
